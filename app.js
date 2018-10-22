9bb984a1890e9b0a40a5f95d6578688ec88a4f8a
Anthony Damico <anthony.damico721@gmail.com>
2018-10-22 14:07:06 -0400

    added venue photo API call

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 'use strict'
+
 //declare object to store all data retrieved by API calls
 const STATE = {
   venueSearch: null,
@@ -6,11 +7,12 @@ const STATE = {
   venuePhotos: null
 };
 
-//declare constants required API URL endpoints
-const venueSearchEndpoint = 'https://api.foursquare.com/v2/venues/search';
+//declare counter to use when rendering venue search result buttons
+let counter = 0;
 
 //make call to retreive venue search data & update STATE object
 function getVenueSearchDataFromApi(searchTerm) {
+    const venueSearchEndpoint = 'https://api.foursquare.com/v2/venues/search'; 
     const settings = {
       url: venueSearchEndpoint,
       data: {
@@ -20,34 +22,44 @@ function getVenueSearchDataFromApi(searchTerm) {
         client_secret: 'RWKFIU2OXR4TD2IJGEOWXUOFTW3ZMOPFAV4HGKHHNMCIIV2Q',
         categoryId: '4bf58dd8d48988d1e2941735',
         query: 'beach',
-        limit: 20
+        limit: 10
       },
       dataType: 'json',
-      type: 'GET',
+      type: 'GET'
     };
 
     $.ajax(settings).then((results) => {
       STATE.venueSearch = results.response;
       displayVenueSearchData();
     });
+
+    //reset counter
+    counter = 0;
+
     //test to see what current value of STATE is
-    console.log(STATE); 
+    console.log(STATE);
   }
 
-  //render HTML template for each venue search result
-  function renderVenueSearchResults(result) {
-      const venueName = result.name;
-      const addressArray = result.location.formattedAddress;
-      const address = addressArray.join(', ');
-      return `
-      <div>
-        <h2>${venueName}</h2>
-        <h3>${address}</h3>
-        <button class="js-details-btn">Details</button>
-      </div>
-      <br>
-      `
-  }
+//make call to retreive venue photo data & update STATE object
+function getVenuePhotosFromApi(venueId) {
+  const venuePhotosEndpoint = `https://api.foursquare.com/v2/venues/${venueId}/photos`;
+  const settings = {
+    url: venuePhotosEndpoint,
+    data: {
+      v: '20181018',
+      client_id: 'P0MLS2UIEUF3FNS21HRUR3HYUPBVRYZ2QR2QTXH1WXR5YKE4',
+      client_secret: 'RWKFIU2OXR4TD2IJGEOWXUOFTW3ZMOPFAV4HGKHHNMCIIV2Q',
+      limit: 10
+    },
+    dataType: 'json',
+    type: 'GET'
+  };
+  
+  $.ajax(settings).then((results) => {
+    STATE.venuePhotos = results.response;
+    displayVenuePhotos();
+  });
+}
 
   //pass each venue search result through the HTML rendering function & make results section displayable
   function displayVenueSearchData() {
@@ -56,7 +68,41 @@ function getVenueSearchDataFromApi(searchTerm) {
       $('.js-results').prop('hidden', false);
   }
 
-  //listen for user input & submit
+  //pass each venue photo result through the HTML rendering function & make results displayable
+  function displayVenuePhotos() {
+    const photos = STATE.venuePhotos.photos.items.map((item) => renderVenuePhotos(item));
+    $('.js-results').html(photos);
+  }
+
+    //render HTML template for each venue search result
+    function renderVenueSearchResults(result) {
+      const venueName = result.name;
+      const addressArray = result.location.formattedAddress;
+      const address = addressArray.join(', ');
+      counter++;
+      return `
+      <div>
+        <h2>${venueName}</h2>
+        <h3>${address}</h3>
+        <button class="js-details-btn-${counter}">Details</button>
+      </div>
+      <br>
+      `
+  }
+
+  //render HTML template for each venue photo
+  function renderVenuePhotos(result) {
+    const prefix = result.prefix;
+    const suffix = result.suffix;
+    const size = '500x500'
+    return `
+    <div>
+      <img src="${prefix}${size}${suffix}" alt="Beach photo">
+    </div>
+    `
+  }
+
+  //listen for user search input on submit
   //run venue search API call
   function submitVenueSearch() {
     $('.js-search-form').on('submit', function(event) {
@@ -71,5 +117,18 @@ function getVenueSearchDataFromApi(searchTerm) {
     });
   }
 
-//document ready function
+  //listen for user click on venue 1 button
+  //run detail API calls
+  function submitVenue1DetailsButton () {
+    $('body').on('click', '.js-details-btn-1', function(event) {
+      const venueId = STATE.venueSearch.venues[0].id;
+      getVenuePhotosFromApi(venueId);
+
+      //test to see what current value of STATE is
+      console.log(STATE);
+    });
+  }
+
+//document ready functions
   $(submitVenueSearch);
+  $(submitVenue1DetailsButton);
